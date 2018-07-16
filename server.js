69215ea56ee9ebc5e4576e8b5f0aaabd9471c185
srihar5ha <34073035+srihar5ha@users.noreply.github.com>
2018-07-17 01:25:10 +0530

    middleware logger test

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ var express=require('express');
 var bodyparser=require('body-parser');
 var app=express();
 
+
+// middle ware
+
+var logger=function(req,res,next){
+    console.log("logging");
+    next();
+}
+app.use(logger);
+
+
 app.get('/',function(req,res){
     // console.log("inside get");
     res.send("hello world");    
