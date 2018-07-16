3d8af97344f0e02aa39151cde7c55d33d9a8451b
srihar5ha <34073035+srihar5ha@users.noreply.github.com>
2018-07-17 01:39:31 +0530

    res.json method

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
 var express=require('express');
-var bodyparser=require('body-parser');
+var bodyParser=require('body-parser');
+var path=require('path');
 var app=express();
 
 
 // middle ware
 
-var logger=function(req,res,next){
-    console.log("logging");
-    next();
-}
-app.use(logger);
+// var logger=function(req,res,next){
+//     console.log("logging");
+//     next();
+// }
+// app.use(logger);
 
+
+// bodyparser middle ware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+
+//static path
+// app.use(express.static(path.join(__dirname,'public')));
+
+var person={
+    'name':'harsha',
+    'id':123
+}
 
 app.get('/',function(req,res){
     // console.log("inside get");
-    res.send("hello world");    
+    res.json(person);    
 });
 
 app.listen(5000,function(){
